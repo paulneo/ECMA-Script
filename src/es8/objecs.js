d5d5daff5d7bc8dd7ef9f0d833617b59463d1d91
Paulneo <jpaulneo2k@gmail.com>
2022-10-20 15:33:22 -0500

    es8

---
+++ src/es8/objecs.js
@@ -0,0 +1,10 @@
+const countries = {
+  PE: 'Peru',
+  MX: 'Mexico',
+  CO: 'Colombia',
+  CL: 'Chile',
+};
+
+console.log(Object.entries(countries));
+console.log(Object.values(countries));
+console.log(Object.keys(countries));
---
+++ src/es8/trailing-commas.js
@@ -0,0 +1,4 @@
+const array = [24, 62, 46, 24, , , 47];
+
+console.log(array);
+console.log(array.length);
